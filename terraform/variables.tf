10496d9fc116052aa62b750f9e11eaaf2d7f2fac
enam1977 <54340800+enam1977@users.noreply.github.com>
2021-12-03 11:22:12 -0600

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -26,10 +26,10 @@ terraform {
 
 # Provider Block of azure
 provider "azurerm" {
-  tenant_id       = var.tenant_id
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
+  //tenant_id       = var.tenant_id
+  //subscription_id = var.subscription_id
+  //client_id       = var.client_id
+  //client_secret   = var.client_secret
   features {}
 }
 

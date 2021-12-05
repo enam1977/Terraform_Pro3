a19cc9afb45a0ec3678691d1a7f5c9d14829d47a
enam1977 <54340800+enam1977@users.noreply.github.com>
2021-12-05 16:26:05 -0600

    Update login.py

--- selenium/login.py
+++ selenium/login.py
@@ -1,83 +1,57 @@
 # #!/usr/bin/env python
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
-import logging
+import datetime
 
-logging.basicConfig(
-    format='%(asctime)s %(levelname)-8s %(message)s',
-    level=logging.INFO,
-    datefmt='%Y-%m-%d %H:%M:%S')
 
-# driver = webdriver.Chrome()
-# print ('Browser started successfully. Navigating to the demo page to login.')
-# driver.get('https://www.saucedemo.com/')
-
+def timestamp():
+    ts = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return (ts + '\t')
 
 # Start the browser and login with standard_user
 def login(user, password):
-    logging.info('Starting the browser...')
-    # --uncomment when running in Azure DevOps.
+    print(timestamp() + 'Starting the browser...')
     options = ChromeOptions()
-    # solve DevToolsActivePort
-    options.add_argument("--headless")
     options.add_argument('--no-sandbox')
-    # options.add_argument('--disable-dev-shm-usage')
-    driver = webdriver.Chrome('/usr/bin/chromedriver', options=options)
-    # print ('Browser started successfully. Navigating to the demo page to login.')
+    options.add_argument("--headless") 
+    driver = webdriver.Chrome(options=options)
+    print(timestamp() + 'Browser started successfully. Navigating to the demo page to login.')
     driver.get('https://www.saucedemo.com/')
-    # input user, password
-    driver.find_element_by_css_selector("input[id=user-name]").send_keys(user)
-    driver.find_element_by_css_selector(
-        "input[id=password]").send_keys(password)
-    driver.find_element_by_css_selector("input[id=login-button]").click()
-    product_label = driver.find_element_by_css_selector(
-        "span[class=title").text
-    assert "PRODUCTS" in product_label
-    logging.info('Login with username: {:s} and password: {:s} successfully.'.format(
-        user, password))
+    # login
+    driver.find_element_by_css_selector("input[id='user-name']").send_keys(user)
+    driver.find_element_by_css_selector("input[id='password']").send_keys(password)
+    driver.find_element_by_id("login-button").click()
+    product_label = driver.find_element_by_css_selector("div[class='product_label']").text
+    assert "Products" in product_label
+    print(timestamp() + 'Login with username {:s} and password {:s} successfully.'.format(user, password))
     return driver
 
-# add all items to cart
+def add_cart(driver, n_items):
+    for i in range(n_items):
+        element = "a[id='item_" + str(i) + "_title_link']"  # Get the URL of the product
+        driver.find_element_by_css_selector(element).click()  # Click the URL
+        driver.find_element_by_css_selector("button.btn_primary.btn_inventory").click()  # Add the product to the cart
+        product = driver.find_element_by_css_selector("div[class='inventory_details_name']").text  # Get the name of the product from the page
+        print(timestamp() + product + " added to shopping cart.")  # Display message saying which product was added
+        driver.find_element_by_css_selector("button.inventory_details_back_button").click()  # Click the Back button
+    print(timestamp() + '{:d} items are all added to shopping cart successfully.'.format(n_items))
 
-
-def add_items(driver, nums):
-    for i in range(nums):
-        prod = "a[id='item_"+str(i)+"_title_link']"
-        # print(prod)
-        driver.find_element_by_css_selector(prod).click()
-        driver.find_element_by_css_selector(
-            'button.btn_primary.btn_inventory').click()
-        product = driver.find_element_by_css_selector(
-            "div[class='inventory_details_name large_size']").text
-        logging.info("Add " + product + " to cart successfully!")
-        # back to main menu
-        driver.find_element_by_css_selector(
-            'button[id=back-to-products]').click()
-    logging.info(str(nums)+" products has been added to the cart.")
-
-
-def remove_items(driver, nums):
-    for i in range(nums):
-        prod = "a[id='item_"+str(i)+"_title_link']"
-        # print(prod)
-        driver.find_element_by_css_selector(prod).click()
-        driver.find_element_by_css_selector(
-            'button.btn_secondary.btn_inventory').click()
-        product = driver.find_element_by_css_selector(
-            "div[class='inventory_details_name large_size']").text
-        logging.info("Remove " + product + " from the cart successfully!")
-        # back to main menu
-        driver.find_element_by_css_selector(
-            'button[id=back-to-products]').click()
-    logging.info(str(nums)+" products has been removed from the cart.")
+def remove_cart(driver, n_items):
+    for i in range(n_items):
+        element = "a[id='item_" + str(i) + "_title_link']"
+        driver.find_element_by_css_selector(element).click()
+        driver.find_element_by_css_selector("button.btn_secondary.btn_inventory").click()
+        product = driver.find_element_by_css_selector("div[class='inventory_details_name']").text
+        print(timestamp() + product + " removed from shopping cart.")  # Display message saying which product was added
+        driver.find_element_by_css_selector("button.inventory_details_back_button").click()
+    print(timestamp() + '{:d} items are all removed from shopping cart successfully.'.format(n_items))
 
 
 if __name__ == "__main__":
-    USERNAME = 'standard_user'
-    PASSWORD = 'secret_sauce'
-    driver = login(USERNAME, PASSWORD)
-    # driver.find_element_by_css_selector("a[id='item_1_title_link']").click()
-    nums = 6
-    add_items(driver, nums)
-    remove_items(driver, nums)
-    logging.info('Selenium tests are all successfully completed!')
+    N_ITEMS = 6
+    TEST_USERNAME = 'standard_user'
+    TEST_PASSWORD = 'secret_sauce'
+    driver = login(TEST_USERNAME, TEST_PASSWORD)
+    add_cart(driver, N_ITEMS)
+    remove_cart(driver, N_ITEMS)
+    print(timestamp() + 'Selenium tests are all successfully completed!')
